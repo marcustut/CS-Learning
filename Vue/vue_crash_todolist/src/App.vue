1e90e36771a9ab3d3896d72f02f6a0d556b2d726
Marcus Lee <marcustutorial@hotmail.com>
2020-07-04 03:06:05 +0800

    Learned Vue Crash Course by Traversy Media

---
+++ Vue/vue_crash_todolist/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav a {
+  font-weight: bold;
+}
+
+#nav a.router-link-exact-active {
+  color: #fff;
+}
+</style>
